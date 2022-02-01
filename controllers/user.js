3c4a0ac616bd9b875bc3580f2cb6fafdde257ea2
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2022-02-01 02:41:32 +0100

    get users controller and route

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,13 @@ export const createUser = async (req, res) => {
         res.status(500).json({ message: 'Something went wrong.' });
     }
 }
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { createUser } from '../controllers/user.js';
+import { createUser, getUsers } from '../controllers/user.js';
 
 const router = express.Router();
 
 router.post('/user', createUser);
+router.get('/users', getUsers);
 
 export default router;
